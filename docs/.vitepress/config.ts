1e92bd92c93a48dff67c19737a5cec0c58bdcfa6
Eureka-24 <ouyangqiu2002@163.com>
2026-06-02 19:28:21 +0800

    fix: 添加 favicon.ico 引用，修复标签栏图标不生效

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
       { rel: "icon", type: "image/svg+xml", href: "/teek-logo-mini.svg" },
     ],
     ["link", { rel: "icon", type: "image/png", href: "/teek-logo-mini.png" }],
+    ["link", { rel: "icon", href: "/favicon.ico" }],
     ["meta", { property: "og:type", content: "website" }],
     ["meta", { property: "og:locale", content: "zh-CN" }],
     ["meta", { property: "og:title", content: "Teek | VitePress Theme" }],
